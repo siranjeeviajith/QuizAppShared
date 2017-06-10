d2d3bb88472b9ca1b52ae8f2dde359a3056dc591
amandeepfull <amandeep.santokh@conversionsupport.com>
2017-06-10 16:22:13 +0530

    fetchDataByBatch method completed

--- Backend/src/main/java/com/fullLearn/service/FullLearnService.java
+++ Backend/src/main/java/com/fullLearn/service/FullLearnService.java
@@ -18,8 +18,10 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fullLearn.helpers.HTTP;
 import com.google.appengine.api.datastore.Cursor;
 import com.google.appengine.api.datastore.QueryResultIterator;
+import com.google.appengine.repackaged.org.joda.time.DateTime;
 import com.googlecode.objectify.ObjectifyService;
 import com.fullLearn.beans.LearningStats;
 import com.googlecode.objectify.cmd.Query;
@@ -33,7 +35,7 @@ public class FullLearnService {
     long countMonth;
     long countYear;
 
-    /**/;
+
     public static boolean fetchUserDetails() throws  IOException {
         String cursorStr = null;
         do {
@@ -50,13 +52,55 @@ public class FullLearnService {
         return true;
     }
 
-	public static void fetchDataByBatch(QueryResultIterator contacts)
-	{
+
+	public static void fetchDataByBatch(QueryResultIterator contacts) throws IOException {
 		//To do for iterating and getting data for each user by Calling HTTP class in helper package
 
-		///  To do for calling method to store data in datastore for each user
+        Date d = new Date();// current date
+        long endDate=d.getTime();// endDate for fetching user data
+        Date dateBefore = new Date(d.getTime() - 1 * 24 * 3600 * 1000  );
+        long startDate=dateBefore.getTime();// start date for fetching user data
+
+        while(contacts.hasNext())
+        {
+            LearningStats contact= (LearningStats) contacts.next();
+                // email will be dynamic for contacts pojo
+            ///// Start time will be dynamic and will be yesterdays date of event and endTime will also be dynamic and and will current time .
+
+            String data=   HTTP.getUserActivities("shaikanjavali.mastan@a-cti.com",startDate,endDate);
+            ObjectMapper objectmapper=new ObjectMapper();
+            Map<String,Object> dataMap=new HashMap<String,Object>();
+            dataMap=objectmapper.readValue(data,new TypeReference<Map<String,Object>>(){});
+
+         System.out.println("data is :"+data);
+           LearningStats entry= MapUserDataAfterFetch(dataMap);
+
+            ///  To do for calling method to store data in datastore for each user
+        }
+
+
+
 	}
 
+
+	public static LearningStats MapUserDataAfterFetch(Map dataMap){
+        // properties of LearningStats pojo
+        //id
+        //userid
+        //minutes
+        //challenges completed
+        //frequency
+        //endTime
+        // startTime
+        LearningStats entry=new LearningStats();
+        if((boolean)dataMap.get("response") && dataMap.get("status").equals("Success"))
+        {
+
+            //// To do mapping is to be done here to Learning Stats
+
+        }
+return entry;
+    }
 
 	/*
 9:41 PM	Error running fl backend run: No task to execute is specified
