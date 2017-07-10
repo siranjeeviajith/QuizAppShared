214e65eb9be954f0f3a98cf8ff4627a3ee3a2e88
amandeepfull <amandeep.santokh@conversionsupport.com>
2017-07-10 18:41:11 +0530

    code refracted

--- Backend/src/main/java/com/fullLearn/services/AllAverageStatsServices.java
+++ Backend/src/main/java/com/fullLearn/services/AllAverageStatsServices.java
@@ -12,42 +12,67 @@ public class AllAverageStatsServices {
 
     public Map<String, Object> getLearningStats(String type, String order, int limit, String cursorStr)
     {
+
+        Query<LearningStatsAverage> userDataQuery =ofy().load().type(LearningStatsAverage.class);
         Map <String, Object> response = null;
 
-        if(type.equals("4"))
+        if(type.equals("4") && order.equals("asce"))
         {
-            if(order.equals("asce"))
-            {
-                order="fourWeekAvg";
-                 response=getUserDataBylimit(cursorStr,order,limit);
-               }
-            else
-            {
-                order="-fourWeekAvg";
-                response=getUserDataBylimit(cursorStr,order,limit);
-            }
+            userDataQuery =ofy().load().type(LearningStatsAverage.class).order("fourWeekAvg").limit(limit);
 
-            return response;
         }
-        else if(type.equals("12"))
+        else if(type.equals("4") && order.equals("desc"))
         {
-            if(order.equals("asce"))
-            {
-                order="twelveWeekAvg";
-                response=getUserDataBylimit(cursorStr,order,limit);
-                }
-            else
-            {
-                order="-twelveWeekAvg";
-                response=getUserDataBylimit(cursorStr,order,limit); }
-            return response;
+            userDataQuery =ofy().load().type(LearningStatsAverage.class).order("-fourWeekAvg").limit(limit);
+        }
+        else if(type.equals("12") && order.equals("asce"))
+        {
+
+            userDataQuery =ofy().load().type(LearningStatsAverage.class).order("twelveWeekAvg").limit(limit);
         }
 
-        return null;
+        else if(type.equals("12") && order.equals("desc"))
+        {
+            userDataQuery =ofy().load().type(LearningStatsAverage.class).order("-twelveWeekAvg").limit(limit);
+
+        }
+           response= getUserDataBylimit(userDataQuery,cursorStr);
+
+
+        return response;
 
     }
 
-    private Map<String, Object> getUserDataBylimit(String cursorStr, String order, int limit) {
+    private Map<String, Object> getUserDataBylimit(Query<LearningStatsAverage> userDataQuery, String cursorStr) {
+
+
+
+        if (cursorStr != null)
+            userDataQuery = userDataQuery.startAt(Cursor.fromWebSafeString(cursorStr));
+
+        QueryResultIterator<LearningStatsAverage> iterator = userDataQuery.iterator();
+
+        List<LearningStatsAverage> userDataList = userDataQuery.list();
+
+        while(iterator.hasNext())
+        {
+            iterator.next();
+        }
+
+        cursorStr = iterator.getCursor().toWebSafeString();
+
+        Map<String,Object> response = new HashMap();
+        response.put("data",userDataList);
+        response.put("error", null);
+        response.put("response", true);
+        response.put("cursor",cursorStr);
+        return response;
+
+    }
+
+
+
+  /*  private Map<String, Object> getUserDataBylimit(String cursorStr, String order, int limit) {
 
 
 
@@ -55,27 +80,8 @@ public class AllAverageStatsServices {
             Query<LearningStatsAverage> userDataQuery =ofy().load().type(LearningStatsAverage.class).order(order).limit(limit);
 
 
-        if (cursorStr != null)
-                userDataQuery = userDataQuery.startAt(Cursor.fromWebSafeString(cursorStr));
-
-            QueryResultIterator<LearningStatsAverage> iterator = userDataQuery.iterator();
-
-            List<LearningStatsAverage> userDataList = userDataQuery.list();
-
-            while(iterator.hasNext())
-            {
-                iterator.next();
-            }
-
-            cursorStr = iterator.getCursor().toWebSafeString();
-
-            Map<String,Object> response = new HashMap();
-            response.put("data",userDataList);
-            response.put("error", null);
-            response.put("response", true);
-            response.put("cursor",cursorStr);
-                return response;
 
 
-    }
+
+    }*/
 }
