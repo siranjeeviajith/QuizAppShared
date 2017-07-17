408ed0965f9845232a255e15a200c8680c4806e3
amandeepfull <amandeep.santokh@conversionsupport.com>
2017-07-17 16:42:34 +0530

    changes made according to comments

--- Backend/src/main/java/com/fullLearn/helpers/SaveContactsHelper.java
+++ Backend/src/main/java/com/fullLearn/helpers/SaveContactsHelper.java
@@ -15,7 +15,7 @@ import com.fullLearn.beans.LearningStatsAverage;
 public class SaveContactsHelper {
     public void saveContacts(ArrayList<Contacts> contacts) throws IOException {
 
-        List<Contacts> saveContactList = new ArrayList();
+        List<Contacts> saveContactList = new ArrayList<>();
         List<String> deleteContactsList = new ArrayList<>();
         Iterator contactsIterator = contacts.iterator();
         while (contactsIterator.hasNext()) {
@@ -30,11 +30,11 @@ public class SaveContactsHelper {
 
 
         }
-        if (saveContactList.isEmpty()) {
+        if (!saveContactList.isEmpty()) {
             System.out.println("save");
             ofy().save().entities(saveContactList).now();
         }
-        if (deleteContactsList.isEmpty()) {
+        if (!deleteContactsList.isEmpty()) {
             System.out.println("deleting contacts size: "+deleteContactsList.size());
             ofy().delete().type(Contacts.class).ids(deleteContactsList).now();
             ofy().delete().type(LearningStatsAverage.class).ids(deleteContactsList).now();
